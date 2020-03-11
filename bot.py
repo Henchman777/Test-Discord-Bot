7ee99bbe89e4c0cf8d100e0efd4da207a8a2c7ff
Henchman <calumhenman7@gmail.com>
2020-03-11 11:29:19 +1000

    Next video

--- bot.py
+++ bot.py
@@ -7,4 +7,16 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('Bot is ready.')
 
-client.run('Njg3MDk1Nzc4OTA0Mzc1MzA5.Xmgx3w.vQgtpDBP4ZUBJo2LW0AQsmvSF6Q')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
+
+
+
+client.run('Njg3MDk1Nzc4OTA0Mzc1MzA5.Xmg80Q.slrKk5sIGtkkAld_tqkFWAMhd6s')
