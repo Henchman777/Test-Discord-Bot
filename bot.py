2a6e7ed4b3fa225039ac0150726561d18229c70a
Henchman <calumhenman7@gmail.com>
2020-03-11 14:11:48 +1000

    FInished Training vidoes

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from itertools import cycle
 
 import random
 
@@ -10,23 +11,84 @@ load_dotenv()
 API_KEY = os.getenv('DISCORD_TOKEN')
 SERVER_NAME = os.getenv('DISCORD_GUILD')
 
-
 client = commands.Bot(command_prefix = '.')
 
-@client.event
-async def on_ready():
-    print('Bot is ready.')
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
 
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+status = cycle(['Status 1', 'Status 2'])
+
+#@tasks.loop(seconds=10)
+#async def change_status():
+#    await client.change_presence(activity=discord.Game(next(status)))
+
+
+#@client.event
+#async def on_ready():
+#    print('Bot is ready.')
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Command not found.')
 
 
 @client.command()
-async def clear(ctx, amount=5):
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount : int):
     await ctx.channel.purge(limit=amount)
 
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('Please specify an amount of messages to delete')
 
-@client.command(aliases=['Ping'])
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency*1000)}ms')
+def is_it_me(ctx):
+    return ctx.author.id == 2307823758932745
+
+
+@client.command()
+@commands.check()
+async def example(ctx):
+    await ctx.send(f'Hi i'm {ctx.author}')
+
+
+#@client.command(aliases=['Ping'])
+#async def ping(ctx):
+#    await ctx.send(f'Pong! {round(client.latency*1000)}ms')
 
 @client.command(aliases=['8ball'])
 async def _8ball(ctx, *, question):
@@ -52,15 +114,14 @@ async def _8ball(ctx, *, question):
                 'You may rely on it.']
     await ctx.send(f'Question: {question} \n Answer: {random.choice(responses)}')
 
-#@client.event
-#async def on_member_join(member):
-#    print(f'{member} has joined a server.')
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
 
 
-#@client.event
-#async def on_member_remove(member):
-#    print(f'{member} has left a server.')
-
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
 
 
 client.run(API_KEY)
---
+++ cogs/example.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands, tasks
+from itertools import cycle
+
+class Example(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        #change_status.start()
+        #await self.client.change_presence(status=discord.Status.idle, activity=discord.Game('Eating Lunch'))
+        print('Bot is ready.')
+
+    # Commands
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('Pong!')
+
+
+#status = cycle(['Status 1', 'Status 2'])
+#@tasks.loop(seconds=10)
+#async def change_status():
+#    await client.change_presence(activity=discord.Game(next(status)))
+
+
+def setup(client):
+    client.add_cog(Example(client))
